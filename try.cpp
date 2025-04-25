ebabdbcc0118cca977cf659181cd2c9905362f18
Hardanu <hardanuilham2@gmail.com>
2025-04-25 13:45:33 +0700

    looping

--- try.cpp
+++ try.cpp
@@ -4,4 +4,10 @@ using namespace std;
 int main(){
     cout<<"Hello World";
     cout<<"\nHalaman Satu";
+    cout<<"\nHalaman Dua\n";
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<"Looping ke-"<<i+1<<endl;
+    }
+    
 }
